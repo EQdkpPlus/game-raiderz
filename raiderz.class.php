4b21e32c5a6a1a84e3ea647efbcddce531874c7a
Simon Wallmann <s.wallmann@kompsoft.de>
2015-01-08 20:29:30 +0100

    Bump version to 1.0.0

--- raiderz.class.php
+++ raiderz.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('raiderz')) {
 	class raiderz extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '0.1';
+		public $version				= '1.0.0';
 		protected $this_game		= 'raiderz';
 		protected $types			= array('classes', 'races', 'filters');
 		protected $classes			= array();
